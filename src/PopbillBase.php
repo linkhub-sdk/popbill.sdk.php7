e15e1eda9301a67822efefc70c040140007b9f7c
1Jae <wjkim@linkhub.co.kr>
2021-07-02 17:53:21 +0900

    added GetPaymentURL, GetUseHistoryURL API

--- src/PopbillBase.php
+++ src/PopbillBase.php
@@ -158,6 +158,21 @@ class PopbillBase
         $response = $this->executeCURL('/?TG=CHRG', $CorpNum, $UserID);
         return $response->url;
     }
+
+    //팝빌 연동회원 포인트 결제내역 URL
+    public function GetPaymentURL($CorpNum, $UserID)
+    {
+        $response = $this->executeCURL('/?TG=PAYMENT', $CorpNum, $UserID);
+        return $response->url;
+    }
+
+    //팝빌 연동회원 포인트 사용내역 URL
+    public function GetUseHistoryURL($CorpNum, $UserID)
+    {
+        $response = $this->executeCURL('/?TG=USEHISTORY', $CorpNum, $UserID);
+        return $response->url;
+    }
+    
     //가입여부 확인
     public function CheckIsMember($CorpNum, $LinkID)
     {

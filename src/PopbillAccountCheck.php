003f1c5d7d1b7f3b014ddda907daf982e4b08c12
정우석 <wsj@linkhubcorp.com>
2024-09-22 19:52:19 +0900

    V24-08-13 수정

--- src/PopbillAccountCheck.php
+++ src/PopbillAccountCheck.php
@@ -123,7 +123,7 @@ class AccountCheckInfo
     public $bankCode;
     public $accountNumber;
     public $accountName;
-    public $checkDate;
+    public $checkDT;
 
     function fromJsonInfo($jsonInfo)
     {
@@ -133,7 +133,7 @@ class AccountCheckInfo
         isset($jsonInfo->bankCode) ? $this->bankCode = $jsonInfo->bankCode : null;
         isset($jsonInfo->accountNumber) ? $this->accountNumber = $jsonInfo->accountNumber : null;
         isset($jsonInfo->accountName) ? $this->accountName = $jsonInfo->accountName : null;
-        isset($jsonInfo->checkDate) ? $this->checkDate = $jsonInfo->checkDate : null;
+        isset($jsonInfo->checkDT) ? $this->checkDT = $jsonInfo->checkDT : null;
     }
 }
 
@@ -146,7 +146,7 @@ class DepositorCheckInfo
     public $accountName;
     public $identityNumType;
     public $identityNum;
-    public $checkDate;
+    public $checkDT;
 
     function fromJsonInfo($jsonInfo)
     {
@@ -157,7 +157,7 @@ class DepositorCheckInfo
         isset($jsonInfo->accountName) ? $this->accountName = $jsonInfo->accountName : null;
         isset($jsonInfo->identityNumType) ? $this->identityNumType = $jsonInfo->identityNumType : null;
         isset($jsonInfo->identityNum) ? $this->identityNum = $jsonInfo->identityNum : null;
-        isset($jsonInfo->checkDate) ? $this->checkDate = $jsonInfo->checkDate : null;
+        isset($jsonInfo->checkDT) ? $this->checkDT = $jsonInfo->checkDT : null;
     }
 }
 
